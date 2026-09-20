f5f4ec84be02e93234088afe49b68eb71c0e3077
Mark McMillan <admin@cabintechglobal.com>
2026-09-20 14:55:43 -0400

    Set pixel color order to Waveshare colors by default

--- SandboxFX_dev/Firmware/src/config.py
+++ SandboxFX_dev/Firmware/src/config.py
@@ -7,7 +7,7 @@ DEBUG_MODE = False                    # Set to True to enable detailed debug out
 
 I2S_SAMPLE_RATE = 48000               # Must match samole rate of dev pedal!!
 
-#NEOPIXEL_COLOR_ORDER = neopixel.GRB   # Waveshare boards use GRB color scheme
-NEOPIXEL_COLOR_ORDER = neopixel.RGB  # Generic unbranded boards (often with "V1083" legend) use RGB color scheme
+NEOPIXEL_COLOR_ORDER = neopixel.GRB   # Waveshare boards use GRB color scheme
+#NEOPIXEL_COLOR_ORDER = neopixel.RGB  # Generic unbranded boards (often with "V1083" legend) use RGB color scheme
 
 PROGRAM_ALL_ON_STARTUP = False        # Program all .hex files on the drive to the FXCore at every reboot/reset
